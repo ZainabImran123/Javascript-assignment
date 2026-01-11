aabb07d0f2678430bce47ede70453854bf932596
Your Name <your.email@example.com>
2026-01-11 13:43:27 +0500

    javascript

--- chapter17-20/app.js
+++ chapter17-20/app.js
@@ -1,15 +1,9 @@
-var arr = [];
-let matrix = [
-    [0,1,2,3]
-    [1,0,1,2]
-    [2,1,0,1]
-]
-document.write(matrix);
-for (let i = 1; i <= 10; i++){
-    console.log(i);
-}
+var empty = [] 
+console.log(empty);
 
-    
+
+var matrix = [0,1,2,3]
+
 
 fruits = ["apple","banana","mango","orange","strawberry"]
 for (let i = 0; i < fruits.length; i++){
@@ -18,20 +12,44 @@ for (let i = 0; i < fruits.length; i++){
 }
 
 
-var A = ["cake","apple pie","cookie","chips","patties"]
+var food = ["cake","apple pie","cookie","chips","patties"]
 var userInput = prompt("Enter an item to search for:")
-for(let i = 0; i < A.length; i++){
-    if (A[i].toLowerCase() === userInput){
-        found = true;
-        break;
+userInput = userInput.toLowerCase()
+for(var i = 0; i < food.length; i++){
+    if (food[i] === userInput){
+        document.write("Yes this is available in our bakery")
+    }else{
+        document.write("Yes this is available in our bakery")
     }
 }
-if(found){
-    alert(`Yes, "${userInput}"  is found in the list.`)
-}else{
-    alert(`No, "${userInput}"  is found not in the list.`)
+
+for(var i = 1; i<11; i++)
+console.log(i);
+
+
+var val =+prompt("Enter a number or a table",5) || 5
+var len = +prompt("Enter a length for a table",10) || 10
+for(var i = 1; i <= len; i++){
+    document.write(val + " x "+i+" = "+val*i+"<br>")
 }
 
+
+
+for(var i = 1; i<= 15; i++){
+    console.log(i);
+}
+for(var i = 10; i>= 15; i--){
+    console.log(i);
+}
+for(var i = 0; i<= 20; i += 2){
+    console.log(i);
+}
+for(var i = 1; i<= 19; i += 2){
+    console.log(i);
+}
+for(var i = 1; i<= 10; i++){
+    console.log(i * 2 + "k");
+}
 
 
 
