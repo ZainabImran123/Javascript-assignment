03a1bdf703b54eed7ef30f2843c100cd7dda46f5
Your Name <your.email@example.com>
2026-01-27 23:19:04 +0500

    java

--- chapter31-34/app.js
+++ chapter31-34/app.js
@@ -1,7 +1,7 @@
 var date = new Date()
 dateStr = date.toString()
 var time = dateStr.slice()
-console.log(time);
+alert(time);
 
 
 
@@ -36,15 +36,63 @@ if(day === 0 || day === 6){
 
 
 
+var first = new Date(1/1/2026)
+var today = new Date()
+var second = first.getDate() - today.getDay()
+if(day === 15){
+    alert("First fifteen days of the month")
+}else{
+    alert("last days of the month")
+}
 
 
 
 
 
 
+var currentTime = new Date()
+var currentHour = currentTime.getHours()
+if(time < 12){
+    alert("It's am")
+}else{
+    alert("It's pm")
+}
 
 
 
+
+
+var day = new Date(2/18/2026)
+var newDay = new Date(3/6/2026)
+var newDate = day.getDay() - newDay.getDay()
+alert("17 Days have passed since 1st Ramadan");
+
+
+
+var datee = new Date(12/30/2025)
+dateeStr = datee.toString()
+var timee = dateeStr.slice()
+alert(timee);
+
+
+
+
+
+var dateee = new Date(12/5/2025)
+var newDateee = dateee/12/30/24/60/60
+alert(newDateee.toString+"second had passed since 2025")
+
+
+
+//var dateeee = new Date()
+//var oldDateeee = new Date(1/28/1926)
+//alert(oldDateeee)
+var dateeeeeeee = new Date(1/28/2026)
+var dateeee = new Date(1/28/1926)
+var dateeee = dateeeeeeee.getFullYear() - dateeee.getFullYear()
+dateeeeeStr = dateeee.toString()
+var timeee = dateeeeeStr.slice()
+alert(timeee);
 
 
 
