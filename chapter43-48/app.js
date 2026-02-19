463ab051513156df42ee597768a89b345aae4ec9
Your Name <your.email@example.com>
2026-02-19 19:33:59 +0500

    javascript

---
+++ chapter43-48/app.js
@@ -0,0 +1,4 @@
+function bulbClicked(e){
+    console.log(e);
+    e.src = "https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1500w,f_auto,q_auto:best/MSNBC/Components/Photo/_new/081209-light-bulb-03.jpg"
+}
